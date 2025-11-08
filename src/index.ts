c85111c11347a33f64dcbb4093cffe7d4e1625f9
danceone <danceone@danceones-MacBook-Air.local>
2025-11-07 20:03:52 -0800

    fixed a typo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (_req, res) => {
-  res.send('Helloo Express!')
+  res.send('Hello Express!')
 })
 
 app.get('/api/users/:id', (_req, res) => {
